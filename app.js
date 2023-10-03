ac006c1abba4c325ae3d1458b579052022b227e6
Jennifer Tan <housingmatchnyc@gmail.com>
2023-10-03 11:07:34 -0400

    test commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var session = require('express-session');
 var MongoStore = require('connect-mongo');
 
 var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users'); TBD if name is appropriate
+//var userRouter = require('./routes/user')
 
 var app = express();
 
@@ -17,6 +17,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+/*
 app.set('trust proxy', 1);
 app.enable('trust proxy');
 
@@ -40,7 +41,7 @@ app.use(
     })
   })
 );
-
+*/
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -48,7 +49,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-// app.use('/users', usersRouter); TBD if we are going to use this name
+//app.use('/user', userRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
