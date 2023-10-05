a0f3fdf8bae5133d2718d40525ddfd8a840ad3c3
Jennifer Tan <housingmatchnyc@gmail.com>
2023-10-04 21:55:57 -0400

    user profile CRUD

--- routes/user.js
+++ routes/user.js
@@ -1,26 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
+const User = require('../models/User')
+
 //const { isLoggedIn } = require('../middleware/route-guard')
 
-/* GET users listing. */
+/* GET PROFILE */
 router.get('/profile', (req, res) => {
 
   res.render('user/profile.hbs', { user: req.session.user })
 
 });
 
-router.get('/update-profile', (req, res, next) => {
+//EDIT PROFILE
+router.post('/profile/edit', (req, res, next) => { 
+  const userID = req.session.user._id
+  const { fullName, email, password } = req.body
+  // console.log(req.params, req.body)
 
-  res.render('user/update-profile')
+  User.findByIdAndUpdate(userID, { fullName, email, password }, {new: true})
+  .then((updatedProfile) => {
+    req.session.user = updatedProfile  //reflect changes immediately
+    res.redirect("/user/profile")
+  })
+  .catch(error => next(error));
+  });
 
-})
+//DELETE PROFILE
+router.post('/profile/delete', (req, res, next) => { 
+  const userID = req.session.user._id
+  const { fullName, email, password } = req.body
 
-router.post('/update', (req, res, next) => { // unfinished request
-  const { email, password } = req.body
-
-  User.findByIdAndUpdate()
-})
+  User.findByIdAndDelete(userID, { fullName, email, password }, {new: true})
+  .then((deletedProfile) => {
+    if(userID) {
+      req.session.user = deletedProfile
+      alert("Sorry to see you go!")
+      res.redirect("/")
+    } 
+    })
+    .catch(error => next(error))    
+  })
 
 
 
